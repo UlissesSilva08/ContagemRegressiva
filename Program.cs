59447d01d1e633d4dc6eef3465fea5692d1d7352
manunasci <nascimentomanuella73@gmail.com>
2021-08-26 19:04:06 -0300

    Inserindo os comando WriteLine e ReadKey

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.WriteLine("FOGO!");
+            Console.ReadKey();
+        }
+    }
+}
